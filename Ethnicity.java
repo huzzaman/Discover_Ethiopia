9c743f3a8c4186eea18612d7062b6a59778601e9
Ahmed1029Hibet <60030432+Ahmed1029Hibet@users.noreply.github.com>
2020-01-20 23:09:23 +0300

    i have done my part edit it

---
+++ Ethnicity.java
@@ -0,0 +1,14 @@
+package com.example.discoverethiopia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ethnicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ethnicity);
+    }
+}
